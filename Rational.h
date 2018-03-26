8ebb0fbcdf5f2884bfc0f295b3a3c4fde4ab962c
thirdeyechai <thirdeyechai@gmail.com>
2018-03-26 22:44:03 +1100

    Rational header
    Class header containing a lot of TO-DOs

---
+++ Rational.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class rational {
+
+	public:
+		// Constructors & Destructor
+		// Accessor & Mutator methods
+		// Greatest Common Divisor method (Euclidean algorithm)
+		// Simplify method (reducing rational to simplest form)
+		// Overloaded operators (+ - * / < > == !=) 
+
+
+	private:
+		int n, d; // Numerator & Denominator
+}
