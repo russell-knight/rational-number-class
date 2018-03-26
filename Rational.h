a8c485c09d8310e5663ac623c33ae1bb53ecca84
dan <danieleades@hotmail.com>
2018-03-26 18:24:15 +0100

    add member functions

--- Rational.h
+++ Rational.h
@@ -3,9 +3,27 @@
 class Rational {
 
 	public:
-		// Constructors & Destructor
-		// Accessor & Mutator methods
-		// Greatest Common Divisor method (Euclidean algorithm)
+		Rational(int numerator, int denominator)
+		{
+			n= numerator;
+			d=denominator;
+		}
+		
+		int numerator() const
+		{
+			return n;
+		}
+
+		int denominator() const
+		{
+		return d;
+		}
+
+		int GCD(int a, int b)
+		{
+			return b==0?a:GCD(b,a%b);
+		}
+		
 		// Simplify method (reducing rational to simplest form)
 		// Overloaded operators (+ - * / < > == !=) 
 
